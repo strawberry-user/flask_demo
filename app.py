69c668fbc661bcc3f71882cf7a9545e1e2f100c5
hansijie <hansijie@taou.com>
2022-07-07 16:29:55 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask
+from flask import request, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Settings
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/appdb'
+db = SQLAlchemy(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return '<h1>Home</h1>'
+
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return '''<form action="/signin" method="post">
+              <p><input name="username"></p>
+              <p><input name="password" type="password"></p>
+              <p><button type="submit">Sign In</button></p>
+              </form>'''
+
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    # 需要从request对象读取表单内容：
+    if request.form['username']=='admin' and request.form['password']=='password':
+        return '<h3>Hello, admin!</h3>'
+    return '<h3>Bad username or password.</h3>'
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+@app.route('/hello')
+def hellp():
+    return render_template('index.html', name='hansijie')
+
+
+@app.route('/form', methods=['POST', 'GET'])
+def bio_data_form():
+    if request.method == "POST":
+        username = request.form['username']
+        age = request.form['age']
+        email = request.form['email']
+        hobbies = request.form['hobbies']
+        return redirect(url_for('showbio',
+                                username=username,
+                                age=age,
+                                email=email,
+                                hobbies=hobbies))
+    return render_template("bio_form.html")
+
+
+@app.route('/showbio', methods=['GET'])
+def showbio():
+    username = request.args.get('username')
+    age = request.args.get('age')
+    email = request.args.get('email')
+    hobbies = request.args.get('hobbies')
+    return render_template("show_bio.html",
+                           username=username,
+                           age=age,
+                           email=email,
+                           hobbies=hobbies)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
