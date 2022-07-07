dbc5a3b12cca691529e5750577bbf23bc1b3d225
hansijie <hansijie@taou.com>
2022-07-07 23:48:15 +0800

    Add static files

--- app.py
+++ app.py
@@ -4,20 +4,33 @@ from flask import request, render_template, redirect, url_for, escape
 
 app = Flask(__name__)
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+    return render_template('index.html', name=name, movies=movies)
 
 
 @app.route('/signin', methods=['GET'])
 def signin_form():
-    return escape('''<form action="/signin" method="post">
+    return '''<form action="/signin" method="post">
               <p><input name="username"></p>
               <p><input name="password" type="password"></p>
               <p><button type="submit">Sign In</button></p>
-              </form>''')
+              </form>'''
 
 
 @app.route('/signin', methods=['POST'])
