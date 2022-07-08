1b6b000e322d5c95df1678bbf56afb5e603beb29
hansijie <hansijie@taou.com>
2022-07-08 11:27:26 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,87 +1,76 @@
-from flask import Flask
-from flask import request, render_template, redirect, url_for, escape
+import os
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import click
 
 
 app = Flask(__name__)
+# sqlite:////数据库文件的绝对地址
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+# 在扩展类实例化前加载配置
+db = SQLAlchemy(app)
 
-name = 'Grey Li'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+
+# 使用方法：
+# 创建数据库表：flask initdb
+# 删除表后重新创建：flask initdb --drop
+@app.cli.command()  # 注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.')  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')  # 输出提示信息
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
 
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('index.html', name=name, movies=movies)
-
-
-@app.route('/signin', methods=['GET'])
-def signin_form():
-    return '''<form action="/signin" method="post">
-              <p><input name="username"></p>
-              <p><input name="password" type="password"></p>
-              <p><button type="submit">Sign In</button></p>
-              </form>'''
-
-
-@app.route('/signin', methods=['POST'])
-def signin():
-    # 需要从request对象读取表单内容：
-    if request.form['username']=='admin' and request.form['password']=='password':
-        return '<h3>Hello, admin!</h3>'
-    return '<h3>Bad username or password.</h3>'
-
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-
-@app.route('/about')
-def about():
-    return 'The about page'
-
-
-@app.route('/hello')
-def hellp():
-    return render_template('index.html', name='hansijie')
-
-
-@app.route('/form', methods=['POST', 'GET'])
-def bio_data_form():
-    if request.method == "POST":
-        username = request.form['username']
-        age = request.form['age']
-        email = request.form['email']
-        hobbies = request.form['hobbies']
-        return redirect(url_for('showbio',
-                                username=username,
-                                age=age,
-                                email=email,
-                                hobbies=hobbies))
-    return render_template("bio_form.html")
-
-
-@app.route('/showbio', methods=['GET'])
-def showbio():
-    username = request.args.get('username')
-    age = request.args.get('age')
-    email = request.args.get('email')
-    hobbies = request.args.get('hobbies')
-    return render_template("show_bio.html",
-                           username=username,
-                           age=age,
-                           email=email,
-                           hobbies=hobbies)
+def index():
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 
 
 if __name__ == '__main__':
